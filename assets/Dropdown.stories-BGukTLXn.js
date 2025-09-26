4e644b0a4ddd9389bd05ba4ee027e1520800cd6b
NiniKu <niniku1457@google.com>
2025-09-26 17:07:00 +0800

    Updates

--- assets/Dropdown.stories-BGukTLXn.js
+++ assets/Dropdown.stories-BGukTLXn.js
@@ -1,4 +1,4 @@
-import{D as i}from"./Dropdown-DcScMUqx.js";import p from"./DropdownAPIdoc-ButvzNJd.js";import"./index-vRTWSlA6.js";import"./iframe-Dl73BPBn.js";import"./preload-helper-DLLWEBaG.js";const m=({target:e,trigger:r,bindFilteroption:t={},filter:u,filterHandler:d,changeHandler:n=null})=>{if(t.filter=u,t.filterHandler=d,Array.isArray(r))return new i(e,r,t,n);{let a=document.createElement("div");a.innerHTML=`
+import{D as i}from"./Dropdown-Chz_aiI9.js";import p from"./DropdownAPIdoc-1Os8Osex.js";import"./index-vRTWSlA6.js";import"./iframe-COtk0fJ9.js";import"./preload-helper-DLLWEBaG.js";const m=({target:e,trigger:r,bindFilteroption:t={},filter:u,filterHandler:d,changeHandler:n=null})=>{if(t.filter=u,t.filterHandler=d,Array.isArray(r))return new i(e,r,t,n);{let a=document.createElement("div");a.innerHTML=`
             <h3 class="input-label">title</h3>
             <label class="label input-label">
                 <input type="text" class="dropdown-input" />
