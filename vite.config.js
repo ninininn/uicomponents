0fdff5cf9bec100376844e85237533bfb0305d85
niniku <niniku1457@gmail.com>
2026-03-01 18:58:54 +0800

    [setup] setup npm file

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,23 @@
-import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-    plugins: [
-        tailwindcss(),
-    ],
-    // server: {
-    //     host: "172.20.10.2"
-    // }
-    root: ".",
-    build: {
-        outDir: "dist",
-        rollupOptions: {
-            input: "./index.html", // 入口是 index.html
-            external: [], // 這裡先不用特別加
-            output: {
-                // 輸出的檔案命名規則
-                entryFileNames: "[name].js",
-                chunkFileNames: "[name].js",
-                assetFileNames: "[name].[ext]",
-            },
-        },
+  plugins: [tailwindcss()],
+  // server: {
+  //     host: "172.20.10.2"
+  // }
+  root: ".",
+  build: {
+    outDir: "dist",
+    lib: { entry: "src/index.js" },
+    rollupOptions: {
+      input: "./index.html", // 入口是 index.html
+      external: [], // 這裡先不用特別加
+      output: {
+        // 輸出的檔案命名規則
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].js",
+        assetFileNames: "[name].[ext]",
+      },
     },
+  },
 });
